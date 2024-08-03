b5106894401d9a0721b5e015043a53720771441d
itzharshit99 <iharshitjain99@gmail.com>
2024-08-03 13:42:54 +0530

    basic app structure and navbar component

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./titlecards.css";
+const TitleCards = () => {
+  return (
+    <div className='titleCards'>TitleCards</div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./player.css";
+const Player = () => {
+  return (
+    <div className='player'>Player</div>
+  )
+}
+
+export default Player
