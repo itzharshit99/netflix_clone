cb4b32dda266a33d4c080b335403cad34e84ac4b
itzharshit99 <iharshitjain99@gmail.com>
2024-08-03 21:26:32 +0530

    implemented basic routing and added video player

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./titlecards.css";
 import cards_data from "../../assets/cards/Cards_data.js";
+import { Link } from "react-router-dom";
 
 
 const TitleCards = ({title,category}) => {
@@ -33,10 +34,10 @@ const TitleCards = ({title,category}) => {
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
               <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
               <p>{card.original_title}</p>
-            </div>
+            </Link>
           );
         })}
       </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 const Player = () => {
-  return (
-    <div className='player'>Player</div>
-  )
-}
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGEzZjI5MjFjYWExNTM4NmMzODEzZGZlNjI2ZWE1NyIsIm5iZiI6MTcyMjY4MTcyMi45MzE0NDcsInN1YiI6IjY2YWUwN2VmMjU0NDZiMTdkZWMwNTFmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eZEprRy_SchdAxt1xuPptka88jdJlR7YZJ0T5bDjca8",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
 
-export default Player
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
